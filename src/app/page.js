61c470ac55a5bf3b4bd1039279fd3720ca8f9303
Arron Moore <arron.moore93@gmail.com>
2023-10-02 16:25:55 +0100

    Added examples of modules with pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-import MeetingRoomBookForm from "src/modules/meeting-room/components/meeting-room-book-form";
-
 
 export default function Home() {
   return (
     <main>
-      <h1>Here book a meeting room</h1>
-      <MeetingRoomBookForm></MeetingRoomBookForm>
+      <div>
+        <a href="/buildings">Buildings</a>
+      </div>
     </main>
+
   )
 }
